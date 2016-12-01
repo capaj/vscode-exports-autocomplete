bb17555706c10ea10ea62977a5c05781ad35242d
Jiri Spac <capajj@gmail.com>
2016-12-01 09:48:54 +0100

    readme purged

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-let vscode = require('vscode')
+const vscode = require('vscode')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
